f9ac2242f544a1556515006da1cca3be1f94c77e
Abhijitchougule <106859775+Abhijitchougule@users.noreply.github.com>
2022-06-04 12:07:37 +0530

    Update cfg.sh

--- cfg.sh
+++ cfg.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 sudo yum update -y
-sudo amazon-linux-extras install php8.0 mariadb10.5
-cat /etc/system-release
-sudo yum install -y httpd
+sleep 5
+sudo amazon-linux-extras install php8.0 mariadb10.5 -y
+sleep 20
+sudo yum install -y httpd git 
+sleep 5
 sudo systemctl start httpd
+sleep 5
 sudo systemctl enable httpd
+sleep 5
+cd /home/ec2-user
+sleep 5
+git clone https://github.com/Abhijitchougule/AWS.git
+sleep 5
+mkdir inc
+sudo cp /home/ec2-user/AWS/dbinfo.inc /var/www/inc/
+sleep 5
+sudo cp /home/ec2-user/AWS/SamplePage.php /var/www/html/
+sleep 5
+
+ 
 
